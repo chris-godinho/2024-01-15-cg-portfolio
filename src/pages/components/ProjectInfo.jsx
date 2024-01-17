342a79d5e2db9eb85e3559987e175be9ada56a9e
kristbg <kristbg@gmail.com>
2024-01-17 17:53:10 -0500

    chore: added anchor link to project pictures

--- src/pages/components/ProjectInfo.jsx
+++ src/pages/components/ProjectInfo.jsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from "react";
 
 export default function ProjectInfo({ card }) {
-
   // Define state variable and function for light/dark mode
   const [isLightMode, setIsLightMode] = useState(
     document.getElementById("root").classList.contains("light-mode-cg")
@@ -12,7 +11,9 @@ export default function ProjectInfo({ card }) {
   // Watch for changes to light/dark mode
   useEffect(() => {
     const handleMutation = () => {
-      setIsLightMode(document.getElementById("root").classList.contains("light-mode-cg"));
+      setIsLightMode(
+        document.getElementById("root").classList.contains("light-mode-cg")
+      );
     };
 
     const observer = new MutationObserver(handleMutation);
@@ -26,15 +27,19 @@ export default function ProjectInfo({ card }) {
 
   return (
     <div className="project-details-cg">
-      <img
-        src={card.imageUrl}
-        className="project-thumbnail-cg"
-        alt={card.title}
-      />
+      <a href={card.link} target="_blank" rel="noopener noreferrer">
+        <img
+          src={card.imageUrl}
+          className="project-thumbnail-cg"
+          alt={card.title}
+        />
+      </a>
       <p>{card.description}</p>
       <div className="project-link-tray-cg">
         <a href={card.link} target="_blank" rel="noopener noreferrer">
-          <span className="material-symbols-outlined app-link-icon-cg">exit_to_app</span>
+          <span className="material-symbols-outlined app-link-icon-cg">
+            exit_to_app
+          </span>
         </a>
         <a href={card.repoLink} target="_blank" rel="noopener noreferrer">
           <img
